830aecd82ae16d97da7146f0dd383be29cbee8ac
Tiago Ribeiro <95197303+TiagoRibeiro02@users.noreply.github.com>
2023-07-24 21:18:25 +0100

    added some basic things

--- lib/Pages/home.dart
+++ lib/Pages/home.dart
@@ -8,16 +8,126 @@ class home_page extends StatefulWidget {
 }
 
 class _home_pageState extends State<home_page> {
+
+  String username = 'Tiago';
+  int tasks = 0;
+
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('To-do'),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        centerTitle: true,
+        title: Text(
+          'ToDo',
+          style: TextStyle(
+            fontSize: 25.0,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
         //the right one
-        actions: [],
+        actions: [
+          IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.search,
+                size: 40.0,
+              ),
+          )
+        ],
         //left
-        //leading: ,
+        leading: IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.menu,
+              size: 40.0,
+            ),
+        ),
       ),
+
+
+
+      backgroundColor: Colors.cyanAccent,
+      body: Container(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Opacity(
+                  opacity: 1.0,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      margin: EdgeInsets.fromLTRB(62, 8, 50, 0),
+                      child: Row(
+                        children: [
+                          Expanded(
+                              flex: 1,
+                            child: Container(
+                              alignment: Alignment.centerLeft,
+                              child: InkWell(
+                                onTap: () {}, //go to profile
+                                child: Hero(
+                                  tag: 'avatar',
+                                  child: Container(
+                                    width: 60,
+                                    height: 60,
+                                    child: CircleAvatar(
+                                      radius: 30.0,
+                                      backgroundColor: Colors.green,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                          Expanded(
+                            flex: 1,
+                              child: Container(
+                                alignment: Alignment.centerRight,
+                                child: Container(),
+                              )
+                          ),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.fromLTRB(50, 0, 50, 0),
+                      child: Container(
+                        margin: EdgeInsets.only(top: 20.0, left: 12),
+                        child: SingleChildScrollView(
+                          child: Row(
+                            children: [
+                              Flexible(
+                                  child: InkWell(
+                                    onTap: () {}, //profile page
+                                    child: Text(
+                                      'Hello $username',
+                                      style: TextStyle(
+                                        fontSize: 30.0,
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.white,
+                                        overflow: TextOverflow.ellipsis,
+                                      ),
+                                    ),
+                                  )
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      )
+
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:todo_list/Pages/home.dart';
 import 'package:todo_list/Pages/login.dart';
 import 'package:todo_list/Pages/register.dart';
 
 void main() {
   runApp(
     MaterialApp(
-      initialRoute: '/',
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/home',
       routes: {
         '/': (context) => Login(),
         '/register': (context) => Register(),
+        '/home': (context)  => home_page(),
       },
     )
   );
